a631cacb822f40bf2ef68a871ff42fd57dd26470
kelvinessum <kessuman001@st.ug.edu.gh>
2024-06-20 18:22:40 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,10 @@
 /**
  * main - Entry point
  *
- * Description: Prints "Programming is like building a multilingual puzzle"
- *
- * Return: 0 if successful
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-  puts("Programming is like building a multilingual puzzle");
+  puts("\"Programming is like building a multilingual puzzle");
   return (0);
 }
