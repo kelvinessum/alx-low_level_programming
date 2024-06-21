ee48701ef85b36c9d7c3d32d201fbb71fb3f4059
kelvinessum <kessuman001@st.ug.edu.gh>
2024-06-20 18:18:52 -0700

    c programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Description: Prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: 0 if successful
+ */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
-    }
+  puts("Programming is like building a multilingual puzzle");
+  return (0);
+}
