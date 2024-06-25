2bcc0f4085aac843ee107a7e3332768e6550587e
kelvinessum <kessuman001@st.ug.edu.gh>
2024-06-25 16:22:59 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - empty function for the main
+ *
+ *return - 0;
+ */
+
+int main(void)
+{
+	printf("_");
+	printf("p");
+	printf("u");
+	printf("t");
+	printf("c");
+	printf("h");
+	printf("a");
+	printf("r");
+	return 0;
+}
