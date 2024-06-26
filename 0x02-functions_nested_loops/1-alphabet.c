a2b186e6523c924c1f2043cf7f6f92f0dc98be4a
kelvinessum <kessuman001@st.ug.edu.gh>
2024-06-25 17:37:41 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main : prints alphabets in lowercase followed by new line
+ *
+ *return :  (Success)
+ */
+
+int main(void)
+{
+	char a;
+	if (a = 'a' , a <= 'z')
+		a++;
+	_putchar(a);
+	_putchar('\n');
+
+	return(0);
+}
