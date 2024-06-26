923aceb01efb5918a871afd4da29519664157050
kelvinessum <kessuman001@st.ug.edu.gh>
2024-06-26 12:21:38 -0700

    nested loops and functions

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day.
+ *
+ */
+
+void jack_bauer(void)
+{
+	int H, M;
+
+	for (H = 0; H <= 23; H++)
+	{
+		for (M = 0; M <= 59; M++)
+		{
+			_putchar((H / 10) + '0');
+			_putchar((H % 10) + '0');
+			_putchar(':');
+			_putchar((M / 10) + '0');
+			_putchar((M % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
