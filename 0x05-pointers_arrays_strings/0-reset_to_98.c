74ff95c0ffeea937c67dd32fab551b0905e283e2
kelvinessum <kessuman001@st.ug.edu.gh>
2024-07-01 16:51:03 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - takes a pointer to an int as parameter
+ *              updates the value it points to to 98.
+ * @n: integer used
+ * return : Always (success)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
