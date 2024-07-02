286b5a2860455cd9279935621054305de7ddab77
kelvinessum <kessuman001@st.ug.edu.gh>
2024-07-01 17:41:40 -0700

    pointers and strange

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string used
+ */
+void rev_string(char *s)
+{
+	int i, len1, len2;
+	char stg;
+
+	len1 = 0;
+	len2 = 0;
+
+	while (s[len1] != '\0')
+	{
+		len1++;
+	}
+
+	len2 = len1 - 1;
+
+	for (i = 0; i < len1 / 2; i++)
+	{
+		stg = s[i];
+		s[i] = s[len2];
+		s[len2--] = stg;
+	}
+}
