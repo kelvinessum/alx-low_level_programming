4fbaefe7fd3c3e5b5ddcd5eafcf177fc1b306336
kelvinessum <kessuman001@st.ug.edu.gh>
2024-07-04 14:12:11 -0700

    more arrays

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* leet - function that encode a string
+*@str:string that will be encoded
+*Return:returns encoded string
+*/
+
+char *leet(char *str)
+{
+	int i = 0, i2;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+
+	while (str[++i])
+	{
+		for (i2 = 0; i2 <= 7; i2++)
+		{
+			if (str[i] == leet[i2] ||
+			 str[i] - 32 == leet[i2])
+				str[i] = i2 + '0';
+		}
+	}
+	return (str);
+}
