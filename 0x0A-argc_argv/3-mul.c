307e8a648df2233833d8a6f1ca4ea9c8ef8cdfcf
kelvinessum <kessuman001@st.ug.edu.gh>
2024-07-17 19:42:39 -0700

    argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * main - Prints the multiplication of two arguments numbers followed by new line
  * @argc: argument count
  * @argv: argument vector
  * Return: returns 0
  */
-
 int main(int argc, char *argv[])
 {
 	int i, j, mul;
@@ -16,11 +16,12 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-
+	
 	i = atoi(argv[1]);
 	j = atoi(argv[2]);
 	mul = i * j;
 
 	printf("%d\n", mul);
+
 	return (0);
 }
