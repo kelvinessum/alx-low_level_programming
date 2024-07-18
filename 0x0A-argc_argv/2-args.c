836919d6d7802d02e8bc656b2d5423a55878bcb4
kelvinessum <kessuman001@st.ug.edu.gh>
2024-07-17 19:32:11 -0700

    argc

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
@@ -13,9 +12,7 @@ int main(int argc, char *argv[])
 	int i;
 
 	for (i = 0; i < argc; i++)
-	{
 		printf("%s\n", argv[i]);
-	}
 
 	return (0);
 }
