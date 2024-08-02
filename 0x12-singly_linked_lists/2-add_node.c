8d98e9571e0741ce0eaa3aac6c315fcffc725b5d
Kelvin Essuman <kessuman001@st.ug.edu.gh>
2024-08-01 22:55:39 -0700

    singly linked files

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of a list
+ *@head: pointer to the head of the list
+ *@str: string to be added
+ *
+ * Return: returns the address to the new element or NULL
+ * if failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	char *dup;
+	int len;
+	list_t *n_node;
+
+	n_node= malloc(sizeof(list_t));
+	if (n_node==NULL)
+		return (NULL);
+
+	dup = strdup(str);
+	if (dup == NULL)
+	{
+		free(n_nord)
+		return (NULL);
+	}
+	for (len = 0; str[len];)
+		len++;
+
+	n_nord->str = dup;
+	n_nord->len =len;
+	n_nord->next = *head
+
+	*head = n_nord;
+
+	return (new);
+
+}
