93167ca8b63aba000d75b1135d5708f11fbcf8c1
Kelvin Essuman <kessuman001@st.ug.edu.gh>
2024-08-12 19:56:20 -0700

    C file I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,24 +14,22 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t op, wr, rd;
 	char *buffer;
+	op = open(filename, O_RDONLY);
+	buffer = malloc(sizeof(char) * letters);
+	rd = read(op, buffer, letters);
+	wr = write(STDOUT_FILENO, buffer, rd);
 
 	if (filename == NULL)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
-
-	op = open(filename, O_RDONLY);
-	rd = read(op, buffer, letters);
-	wr = write(STDOUT_FILENO, buffer, rd);
 
 	if (op == -1 || rd == -1 || wr == -1 || wr != rd)
 	{
 		free(buffer);
 		return (0);
 	}
-
 	free(buffer);
 	close(op);
 
