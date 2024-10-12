26377ea47cc34b6dd7ebb8cee483c519d4bf1514
Kelvin Essuman <kessuman001@st.ug.edu.gh>
2024-10-11 21:44:36 -0700

    new

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -4,10 +4,8 @@
  * hash_table_set - Add or update an element in a hash table.
  * @ht: A pointer to the hash table.
  * @key: The key to add - cannot be an empty string.
- * @value: The value associated with key.
- *
+ * @value: The value associated with key.*
  * Return: Upon failure - 0.
- *         Otherwise - 1.
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
